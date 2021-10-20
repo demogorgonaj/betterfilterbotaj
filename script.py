0dbd4eea222d9831375c4f813c5d97718b26cc7a
demogorgonaj <85363780+demogorgonaj@users.noreply.github.com>
2021-10-20 18:12:49 +0530

    Update script.py

--- script.py
+++ script.py
@@ -48,16 +48,16 @@ See <i>/help</i> for commands and more details.</b>
 <code>/info userid</code>  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
 
 
-<b>© @TroJanzHEX</b>
+<b>© @worldmoviesaj</b>
 """
 
 
-    ABOUT_MSG = """⭕️<b>My Name : TroJanz Filter Bot</b>
+    ABOUT_MSG = """⭕️<b>My Name : MOVIE HUB HD Filter Bot</b>
 
-⭕️<b>Creater :</b> @TroJanzHEX    
+⭕️<b>Creater :</b> @worldmoviesaj    
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
-⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
+⭕️<b>Library :</b> <a href='https://t.me/AJmovieLINKS'>Pyrogram 1.0.7</a> 
 
 """
